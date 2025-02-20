2ffcaef7d6c4b192b7e23bce5d5c6f9c6a3a4cc2
233325332 <233325332@qq.com>
2025-02-20 10:35:07 +0800

    a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='yamlc',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pyyaml',
